9484076d207fa807a23e05bf547b97b4519ab8ea
John Bender <johnbender@john-benders-macbook-pro-2.local>
2009-08-28 03:00:15 -0400

    nice extra bit of refactor

--- lib/rquery/operations_group.rb
+++ lib/rquery/operations_group.rb
@@ -4,12 +4,9 @@ module RQuery
     
     def initialize(args)
       @ops = Array.new
-      #if the two arguments passed are strings, make a new group
-      if args[0].instance_of? String
-        @ops << args[0] << args[1]
-      else #otherwise make the operation the sum of the passed in operation and an additional
-        @ops += args[0].ops << args[1]
-      end
+      #if the two args passed are strings make a new group
+      #otherwise add the second arg to the previous operation group
+      @ops = args[0].instance_of?(String) ? args[0,2] : (args[0].ops << args[1])
     end
     
     def to_s
